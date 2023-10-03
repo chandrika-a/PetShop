f092ddb3dae45a7651956f1a2d3bf98ca333c2fa
Harshit Goyal <harshitgoyal101@gmail.com>
2023-10-03 14:39:49 +0530

    init

---
+++ PetShop/PetApp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+pet_type_choices = (
+    ("Dog", "Dog"),
+    ("Cat", "Cat")
+)
+
+class Pet(models.Model):
+    name = models.CharField(max_length=20)
+    type = models.CharField(max_length=10, choices=pet_type_choices)
+    color = models.CharField(max_length=10)
+    weight = models.FloatField()
+    age = models.PositiveIntegerField(default=0)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
