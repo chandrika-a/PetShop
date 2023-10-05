59eb8dfadb21568c89295a0779d5f70f69901e93
Harshit Goyal <harshitgoyal101@gmail.com>
2023-10-05 16:34:45 +0530

    get all

--- PetShop/PetApp/views.py
+++ PetShop/PetApp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Pet
 
 # Create your views here.
-def index(request):
-    return render(request, "PetApp/home.html")
+def get_all(request, kind=""):  
+    kind = kind.capitalize()
+    if kind in ["Dog", "Cat"]:  
+        pets = Pet.objects.filter(type=kind)    
+    else:
+        pets = Pet.objects.all()
+    count = pets.count()
+    return render(request, "PetApp/home.html", 
+                  {"pets":pets, "count": count})
+
--- PetShop/PetShop/urls.py
+++ PetShop/PetShop/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from PetApp.views import index
+from PetApp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('<str:kind>/', get_all),
+    path('', get_all),
 ]
